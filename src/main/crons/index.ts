c7c3fb2d3774811cbba8d4d125fcfe9c1dc8194d
Douglas Gomes <douglasgomes.rr@gmail.com>
2024-10-09 22:32:24 -0300

    feat: create fixed wallet expenses cron

--- src/main/crons/index.ts
+++ src/main/crons/index.ts
@@ -1,9 +1,16 @@
 import { SchedulerTaskAdapter } from '@/infra/scheduler/scheduler-task-adapter';
 
 import { makeCreateFixedCreditCardExpenseUseCase } from '../factories/use-cases/make-create-fixed-credit-card-expenses-use-case';
+import { makeCreateFixedWalletExpenseUseCase } from '../factories/use-cases/make-create-fixed-wallet-expenses-use-case';
 
 SchedulerTaskAdapter.schedule(
   makeCreateFixedCreditCardExpenseUseCase(),
   '0 1 * * *', // Every day at 1:00 AM
   'create-fixed-credit-card-expense',
+);
+
+SchedulerTaskAdapter.schedule(
+  makeCreateFixedWalletExpenseUseCase(),
+  '0 1 * * *', // Every day at 1:00 AM
+  'create-fixed-wallet-expense',
 );
